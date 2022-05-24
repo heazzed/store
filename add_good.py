e07bfd25828e9a0e8f24f42b8d394b861e27a3b7
Maxim <maxim.milisov@gmail.com>
2022-05-24 11:48:23 +0300

    added price, type and modification in add_good window

--- add_good.py
+++ add_good.py
@@ -6,7 +6,7 @@ class AddGood(QtWidgets.QDialog):
         super().__init__()
 
         self.setWindowTitle("Добавление товара")
-        self.setGeometry(0, 0, 400, 600)
+        self.setGeometry(0, 0, 400, 400)
         formLayout = QtWidgets.QFormLayout()
         gridLayout = QtWidgets.QGridLayout()
 
@@ -20,11 +20,53 @@ class AddGood(QtWidgets.QDialog):
         formLayout.addRow(self.quantityGoodLabel, self.quantityGoodLineEdit)
         gridLayout.addLayout(formLayout, 0, 0)
 
-        self.ok_button = QtWidgets.QPushButton("Сохранить", self)
-        self.ok_button.clicked.connect(self.save)
+        self.priceBox = QtWidgets.QGroupBox("Цены", self)
 
-        gridLayout.addWidget(self.ok_button, 1, 0)
-        gridLayout.setColumnMinimumWidth(0, 500)
+        self.buyPriceGoodLabel = QtWidgets.QLabel("Закупочная цена", self)
+        self.buyPriceGoodLineEdit = QtWidgets.QLineEdit(self)
+
+        self.salePriceGoodLabel = QtWidgets.QLabel("Розничная цена", self)
+        self.salePriceGoodLineEdit = QtWidgets.QLineEdit(self)
+
+        self.priceLayout = QtWidgets.QVBoxLayout()
+        self.priceLayout.addWidget(self.buyPriceGoodLabel)
+        self.priceLayout.addWidget(self.buyPriceGoodLineEdit)
+        self.priceLayout.addWidget(self.salePriceGoodLabel)
+        self.priceLayout.addWidget(self.salePriceGoodLineEdit)
+        self.priceBox.setLayout(self.priceLayout)
+        gridLayout.addWidget(self.priceBox, 1, 0)
+
+        self.typeBox = QtWidgets.QGroupBox("Тип товара")
+        self.typeButton0 = QtWidgets.QRadioButton("Игрушка")
+        self.typeButton1 = QtWidgets.QRadioButton("Одежда")
+        self.typeButton2 = QtWidgets.QRadioButton("Обувь")
+
+        self.typeLayout = QtWidgets.QVBoxLayout()
+        self.typeLayout.addWidget(self.typeButton0)
+        self.typeLayout.addWidget(self.typeButton1)
+        self.typeLayout.addWidget(self.typeButton2)
+        self.typeBox.setLayout(self.typeLayout)
+        gridLayout.addWidget(self.typeBox, 2, 0)
+
+        self.modificationBox = QtWidgets.QGroupBox("Модификация")
+        self.modificationButton0 = QtWidgets.QRadioButton("Обычный товар")
+        self.modificationButton1 = QtWidgets.QRadioButton("Комплект")
+        self.modificationButton2 = QtWidgets.QRadioButton("Набор")
+
+        self.modificationLayout = QtWidgets.QVBoxLayout()
+        self.modificationLayout.addWidget(self.modificationButton0)
+        self.modificationLayout.addWidget(self.modificationButton1)
+        self.modificationLayout.addWidget(self.modificationButton2)
+        self.modificationBox.setLayout(self.modificationLayout)
+        gridLayout.addWidget(self.modificationBox, 3, 0)
+
+        # button clear
+
+        self.okButton = QtWidgets.QPushButton("Сохранить", self)
+        self.okButton.clicked.connect(self.save)
+
+        gridLayout.addWidget(self.okButton, 4, 0)
+        gridLayout.setColumnMinimumWidth(0, 200)
         self.setLayout(gridLayout)
 
     def save(self):
